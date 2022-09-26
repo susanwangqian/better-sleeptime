65628c5f49e8ce8527c0b44f6f40241651d696cc
Susan Wang <susanwangqian02@gmail.com>
2022-09-26 05:44:01 -0400

    Update the script to calc sleep time based on user input

--- script.js
+++ script.js
@@ -14,9 +14,28 @@ function handleOnClick() {
     let hours = document.getElementById("hours");
     hours.innerHTML = "";
 
+    // Hold option key & select other lines - write in multiple lines (.value) at the same time
+    let hh = document.getElementById("hh").value;
+    let mm = document.getElementById("mm").value;
+    let ampm = document.getElementById("ampm").value;
+
+    /* 
+    debugger in browser: sources tab -> upper left icon; (when easy)
+      set breakpoints; 2nd on the left to step over; 
+      hh = 3 + 12, should be 15; 
+      but hh = 312 after this step -> BUG
+        because: value being read is a string! + concat
+    */
+    // hh = ampm === "PM" ? hh + 12 : hh;
+    hh = ampm === "PM" ? Number.parseInt(hh) + 12 : hh;
+    console.log({hh, mm, ampm});
+
     let now = new Date();
     // allow 14 minutes to fall sleep!
-    now.setMinutes(now.getMinutes() + 14);
+    // now.setMinutes(now.getMinutes() + 14);
+
+    now.setHours(hh);
+    now.setMinutes(mm);
   
     // calculate sleep cycles!
     for (let i = 1; i <= 6; i++) {
